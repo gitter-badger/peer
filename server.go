df7aff8d431b58da2250f6b07ac6435129ceb952
MetalMatze <me@metalmatze.de>
2015-06-10 15:11:39 +0200

    Add martini-contrib/render for json rendering

--- server.go
+++ server.go
@@ -2,10 +2,12 @@ package main
 
 import "github.com/go-martini/martini"
 import "github.com/martini-contrib/gzip"
+import "github.com/martini-contrib/render"
 
 func main() {
 	app := martini.Classic()
 	app.Use(gzip.All())
+	app.Use(render.Renderer())
 
 	app.Get("/", func() string {
 		return "Hello World"
@@ -13,6 +15,10 @@ func main() {
 
 	app.Get("/hello/:name", func(params martini.Params) string {
 		return "Hello " + params["name"]
+	})
+
+	app.Get("/json", func(render render.Render) {
+		render.JSON(200, map[string]interface{}{"hello": "World"})
 	})
 
 	app.Run()
