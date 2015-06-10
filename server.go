32fadce111404332cc2b725818db9d7ca20ac2fb
MetalMatze <me@metalmatze.de>
2015-06-11 01:58:11 +0200

    Improve server.go with new routes

--- server.go
+++ server.go
@@ -1,25 +1,65 @@
 package main
 
-import "github.com/go-martini/martini"
-import "github.com/martini-contrib/gzip"
-import "github.com/martini-contrib/render"
+import (
+	"github.com/PhotoTresor/peer/models"
+	"github.com/jinzhu/gorm"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/gzip"
+	"github.com/martini-contrib/render"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	db gorm.DB
+)
 
 func main() {
+	initDB()
+
 	app := martini.Classic()
 	app.Use(gzip.All())
 	app.Use(render.Renderer())
 
-	app.Get("/", func() string {
-		return "Hello World"
+	app.Get("/photos", func(render render.Render) {
+		photos := []models.Photo{}
+		db.Find(&photos)
+
+		render.JSON(200, photos)
 	})
 
-	app.Get("/hello/:name", func(params martini.Params) string {
-		return "Hello " + params["name"]
+	app.Post("/photos", func(render render.Render) {
+		render.Status(201)
 	})
 
-	app.Get("/json", func(render render.Render) {
-		render.JSON(200, map[string]interface{}{"hello": "World"})
+	app.Get("/photos/:id", func(params martini.Params, render render.Render) {
+		photo := models.Photo{}
+
+		db.First(&photo, params["id"])
+
+		render.JSON(200, photo)
 	})
 
 	app.Run()
+}
+
+func initDB() {
+	var err error
+
+	db, err = gorm.Open("sqlite3", "./phototresor.db")
+
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	db.DB().Ping()
+	db.LogMode(true)
+
+	db.CreateTable(&models.Photo{})
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
