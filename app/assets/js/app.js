455d534457ebc82a0df03f059abd0911020b4b9b
MetalMatze <me@metalmatze.de>
2015-06-12 01:35:38 +0200

    Improve app.js with API call and material ui

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,11 +1,76 @@
 import React from 'react'
+import {AppBar} from 'material-ui'
 
-class Test extends React.Component {
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+var mui = require('material-ui');
+var ThemeManager = new mui.Styles.ThemeManager();
+
+var API = {
+    photos() {
+        return fetch('/photos')
+            .then(this._status)
+            .then(this._json)
+    },
+
+    _status(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response;
+        }
+        throw new Error(response.statusText);
+    },
+
+    _json(response) {
+        return response.json()
+    }
+};
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            photos: []
+        }
+    }
+
+    getChildContext() {
+        return {
+            muiTheme: ThemeManager.getCurrentTheme()
+        };
+    }
+
+    componentDidMount() {
+        API.photos()
+            .then((photos) => {
+                this.setState({
+                    photos: photos
+                })
+            })
+    }
+
     render() {
+        var renderPhoto = function (photo, index) {
+            return (
+                <li key={index}>{photo.ID} - {photo.FileName}</li>
+            )
+        };
+
         return (
-            <div>Hello World, this is React.</div>
+            <div>
+                <AppBar
+                    title="Photos"
+                    showMenuIconButton={false}/>
+                <ul>{this.state.photos.map(renderPhoto)}</ul>
+            </div>
         )
     }
 }
 
-React.render(<Test />, document.getElementById('app'));
+App.childContextTypes = {
+    muiTheme: React.PropTypes.object
+};
+
+React.render(<App />, document.getElementById('app'));
