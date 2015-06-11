545ab8dd78473b8a748185ab20ca9995e8e67ad2
MetalMatze <me@metalmatze.de>
2015-06-11 19:58:33 +0200

    Add watching js files to gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 var browserify = require('gulp-browserify');
 
-gulp.task('default', [
-    'js'
-]);
+gulp.task('default', ['js'], function () {
+    return gulp.watch('assets/js/**', ['js']);
+});
 
 gulp.task("js", function () {
     gulp.src("assets/js/app.js")
