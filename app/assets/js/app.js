dccb32ded46732859255b066ec7636dd289fd2f8
MetalMatze <me@metalmatze.de>
2015-06-17 23:03:12 +0200

    Put GridPhoto into own .js file

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -109,35 +109,6 @@ class App extends React.Component {
     }
 }
 
-class GridPhoto extends React.Component {
-
-    constructor(props) {
-        super(props)
-
-        this.handlePhotoDelete = this.handlePhotoDelete.bind(this);
-    }
-
-    render() {
-        var downloadPath = "/photos/" + this.props.photo.id + "/download";
-        return (
-            <div key={this.props.photo.id}
-                 style={{display: 'inline-block', marginRight: '10px', position: 'relative'}}>
-                <i className="material-icons"
-                    onTouchTap={this.handlePhotoDelete}
-                    style={{position: 'absolute', top: 5, right: 5, color: '#eee'}}>delete</i>
-                <img src={downloadPath} alt={this.props.photo.file_name} height="220"/>
-            </div>
-        )
-    }
-
-    handlePhotoDelete() {
-        API.deletePhoto(this.props.photo.id)
-            .then((response) => {
-                console.log(response);
-            })
-    }
-}
-
 App.childContextTypes = {
     muiTheme: React.PropTypes.object
 };
