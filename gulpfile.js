3676a1169dff3ae5f74cb0c3365d2fcb51fe2cb5
MetalMatze <me@metalmatze.de>
2015-06-17 23:02:49 +0200

    Update package.json and gulpfile to work with es6 modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
-var gulp = require("gulp");
-var babel = require("gulp-babel");
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var babelify = require("babelify");
 
-gulp.task('default', ['js'], function () {
-});
+gulp.task('default', [
+    'js'
+]);
 
-gulp.task('watch', ['js'], function () {
+gulp.task('watch', ['default'], function () {
     return gulp.watch('app/assets/js/**', ['js']);
 });
 
-gulp.task("js", function () {
-    gulp.src("app/assets/js/app.js")
-        .pipe(babel())
-        .pipe(browserify())
-        .pipe(gulp.dest("public"));
+gulp.task('js', function () {
+    var b = browserify({
+        entries: 'app/assets/js/app.js',
+        debug: true
+    });
+
+    return b
+        .transform(babelify)
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .on('error', gutil.log)
+        .pipe(gulp.dest('./public/'));
 });
